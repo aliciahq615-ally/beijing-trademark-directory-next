7c41069de29b0810ef08183e247eff31cdb5aef8
aliciahq615-ally <aliciahq615@gmail.com>
2026-07-12 19:52:51 +0800

    Delete app directory

--- app/api/catalog/route.ts
+++
@@ -1,8 +0,0 @@
-import { getCatalogData } from "@/lib/catalog";
-
-export const dynamic = "force-dynamic";
-
-export async function GET() {
-  const data = await getCatalogData();
-  return Response.json(data);
-}
